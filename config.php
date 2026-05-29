7c788787edfdfd7b7882a6ed27b6909f5d49a7ea
Adriene Amigable <adrienecarrreamigable01@gmail.com>
2026-05-29 10:06:34 +0800

    updated database

--- config.php
+++ config.php
@@ -1,6 +1,6 @@
 
 <?php
-$conn = new mysqli("localhost", "root", "", "indo_lending");
+$conn = new mysqli("localhost", "u859692781_indopacific", "Indopacific051824!", "u859692781_indopacific");
 
 if ($conn->connect_error) {
     die("Connection failed: " . $conn->connect_error);
